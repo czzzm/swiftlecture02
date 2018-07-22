5d356970aa2e471d58ee8ab30ab8189c867fcaa9
zzm <352137171@qq.com>
2018-07-22 16:39:42 +0800

    add dictionary

--- swiftlecture02/swiftlecture02/ViewController.swift
+++ swiftlecture02/swiftlecture02/ViewController.swift
@@ -17,17 +17,43 @@ class ViewController: UIViewController {
         print("hello,swift!")
         var name:String = "zhimin"
         let fullname = name + "zhang"
-        
-        
         lbName?.text=fullname
-        
+        // Array
+        var english = ["A","B","C"]
+        var ints : [Int] = [1,2,3]
+        var floats : Array<Float> = Array(repeating: 0, count:5 )
+        ints.sort()
+//        for index in 0..<3{
+//            print("\( english[index] )")
+//        }
+//        for str in english{
+//            print(str)
+//        }
+        //Dictionary
+        let  dic = [1:"A",2:"C",3:"B"]
+        for( a , b) in dic{print("\(a):\(b)")
+        }
+let dicSorted = dic.sorted(by:{ $0.key < $1.key })
+        for( a, b) in dicSorted{
+            print("\(a):\(b)")
+        }
+        var times : Int = 0
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false, block: { smalltimer in
+            times = times + 1
+            print("timer:\( smalltimer.isValid )")})
+   _ = self.add(first: 1, second: 2)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func add(first:Int, second:Int) -> Int {
+        return first + second
+    }
+
+    }
 
 
-}
+
 
